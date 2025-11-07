e2e87c4c4393015387138f8690f2ddc5e04cf492
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 12:56:38 +0700

    Menambahkan tampilan Scaffold dengan TopAppBar berjudul "Tampil"

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,7 +1,11 @@
 package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -13,4 +17,11 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Perempuan"),
         Pair(first = stringResource(id = R.string.alamat), second = "Kotamobagu"),
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id=R.color.teal_700))
+            )
+        })
 }
