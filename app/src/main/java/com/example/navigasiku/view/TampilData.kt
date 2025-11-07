a207e0da7200bef9488e206d45dcb3e1b0d9934b
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 13:08:24 +0700

    menambahkan iterasi data untuk menampilkan daftar informasi dengan teks bergaya dan pemisah antar item

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +30,15 @@ fun TampilData(
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.SpaceBetween) {
             Column(modifier = Modifier.padding(all= dimensionResource(id=R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy (space= dimensionResource(id = 10dp)))
+                verticalArrangement = Arrangement.spacedBy (space= dimensionResource(id = 10dp))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+            }
         }
     }
 }
