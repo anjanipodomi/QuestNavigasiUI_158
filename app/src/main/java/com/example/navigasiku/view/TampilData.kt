3e53d0f0f3f8db0ce823b8b803812c928f696965
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 13:03:08 +0700

    Menambahkan struktur tampilan utama dengan Column dan pengaturan jarak antar elemen

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,5 +24,11 @@ fun TampilData(
                 title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id=R.color.teal_700))
             )
-        })
+        }) { isiRuang->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(all= dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy (space= dimensionResource(id = 10dp)))
+        }
+    }
 }
