4d7794b762abb7a22bab834d2462e6b1c857764f
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 13:43:12 +0700

    menambahkan struktur dasar DataApp dengan Scaffold dan NavHost untuk menyiapkan navigasi antar layar.

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -2,6 +2,7 @@ package com.example.navigasiku
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -15,6 +16,13 @@ fun DataApp(
     navController: NavHostController = rememberNavController()
 ) {
     Scaffold { isiRuang->
-
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues= isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+            }
+        }
     }
 }
